0b33f4a23140a272d2e2c4cfe9e53abfc86f1db7
sriharsh1103 <prashant.sriharsh@antiersolutions.com>
2024-09-23 17:47:34 +0530

    feature:dsa

--- missingInArray.js
+++ missingInArray.js
@@ -1,11 +1,23 @@
-let arr = [1,2,3,4,5,7,8,10,11,12,13];
+// QUESTIONS => Find the missing number in a given integer array of 1 to 100
+
+// PROBLEM => I CAN ONLY FIND ONE NUMBER IN THIS ARRAY ... IF MORE NUMBER ARE MISSING WE CAN'T FIND IT IN IT
+
+let arr = [1,2,3,4,6,5,7,8,10,11,12,13];
 const findMissingNum = (arr) => {
     // Calculate the expected sum of the first n numbers
-    const n = arr.length + 1;  // Add 1 since one number is missing
+    const n = arr.length + 1; 
+    console.log('n', n)
+
+     // Add 1 since one number is missing
     const expectedSum = (n * (n + 1)) / 2;
+    console.log('expectedSum', expectedSum)
     
     // Calculate the actual sum of the array
     const actualSum = arr.reduce((sum, num) => sum + num, 0);
+    console.log('arr.reduce', arr.reduce);
+    
+    console.log('actualSum', actualSum)
+
     
     // The missing number is the difference between the expected and actual sums
     return expectedSum - actualSum;
